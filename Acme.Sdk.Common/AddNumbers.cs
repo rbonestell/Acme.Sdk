6e02d7cbad38bbd09c2a7cd6710c4ada047211f2
Bobby Bonestell <rbonestell@gmail.com>
2022-03-28 19:46:10 -0600

    Initial commit

---
+++ Acme.Sdk.Common/AddNumbers.cs
@@ -0,0 +1,7 @@
+﻿namespace Acme.Sdk.Common;
+
+public class AddNumbers
+{
+    public int A { get; set; }
+    public int B { get; set; }
+}
---
+++ Acme.Sdk.Math/Controllers/AddController.cs
@@ -0,0 +1,23 @@
+﻿using Acme.Sdk.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Acme.Sdk.Math.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AddController : ControllerBase
+{
+    private readonly ILogger<AddController> _logger;
+
+    public AddController(ILogger<AddController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost()]
+    public int Post(AddNumbers addNumber)
+    {
+        return addNumber.A + addNumber.B;
+    }
+}
+
